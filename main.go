a04cf7c5b6b18c6f8c56bcb9877fdbd75424faa5
kristaponis <a.kristaponis@icloud.com>
2021-01-25 21:43:22 +0200

    Added handler to create person

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 	"net/http"
 	"time"
@@ -12,8 +13,7 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-// Person type contains some basic info
-type Person struct {
+type person struct {
 	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
 	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
@@ -21,12 +21,23 @@ type Person struct {
 
 var client *mongo.Client
 
+func personHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	var p person
+	json.NewDecoder(r.Body).Decode(&p)
+	coll := client.Database("personsdb").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	result, _ := coll.InsertOne(ctx, p)
+	json.NewEncoder(w).Encode(result)
+}
+
 func main() {
 	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
 	opts := options.Client().ApplyURI("mongodb://localhost:27017")
 	client, _ = mongo.Connect(ctx, opts)
 
 	router := mux.NewRouter()
+	router.HandleFunc("/person", personHandler).Methods(http.MethodPost)
 
 	log.Println("Starting application...")
 	log.Fatal(http.ListenAndServe(":8080", router))
