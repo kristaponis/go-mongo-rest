8846962891783ea22df733c6d5d8a5fafd0bc342
kristaponis <a.kristaponis@icloud.com>
2021-01-25 21:22:51 +0200

    Added mongodb connection

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/kristaponis/go-mongo-rest
 
 go 1.15
+
+require (
+	github.com/gorilla/mux v1.8.0 // indirect
+	go.mongodb.org/mongo-driver v1.4.5 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+// Person type contains some basic info
+type Person struct {
+	ID        primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Firstname string             `json:"firstname,omitempty" bson:"firstname,omitempty"`
+	Lastname  string             `json:"lastname,omitempty" bson:"lastname,omitempty"`
+}
+
+var client *mongo.Client
+
 func main() {
-	
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	opts := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, _ = mongo.Connect(ctx, opts)
+
+	router := mux.NewRouter()
+
+	log.Println("Starting application...")
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
