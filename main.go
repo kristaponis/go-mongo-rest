02ed33cf6eb763bbcd094f9b78e30607a65e6cd1
kristaponis <a.kristaponis@icloud.com>
2021-01-26 16:58:38 +0200

    Created handler to get all items in the collection

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -21,7 +22,34 @@ type person struct {
 
 var client *mongo.Client
 
-func personHandler(w http.ResponseWriter, r *http.Request) {
+// getPersonHandler gets all items in the collection
+// /person GET
+func getPersonHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "application/json")
+	var persons []person
+	coll := client.Database("personsdb").Collection("people")
+	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	cursor, err := coll.Find(ctx, bson.M{})
+	if err != nil {
+		http.Error(w, `{"message":"`+err.Error()+`"}`, http.StatusInternalServerError)
+		return
+	}
+	defer cursor.Close(ctx)
+	for cursor.Next(ctx) {
+		var p person
+		cursor.Decode(&p)
+		persons = append(persons, p)
+	}
+	if err := cursor.Err(); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	json.NewEncoder(w).Encode(persons)
+}
+
+// createPersonHandler creates new item in the collection
+// /person POST
+func createPersonHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "application/json")
 	var p person
 	json.NewDecoder(r.Body).Decode(&p)
@@ -37,7 +65,8 @@ func main() {
 	client, _ = mongo.Connect(ctx, opts)
 
 	router := mux.NewRouter()
-	router.HandleFunc("/person", personHandler).Methods(http.MethodPost)
+	router.HandleFunc("/person", getPersonHandler).Methods(http.MethodGet)
+	router.HandleFunc("/person", createPersonHandler).Methods(http.MethodPost)
 
 	log.Println("Starting application...")
 	log.Fatal(http.ListenAndServe(":8080", router))
